5e01a1ae86f81c74693cae1d094caad888250817
PabloRivera <priveras@gmail.com>
2015-08-22 11:24:55 -0500

    updates

--- project/gocart/themes/default/views/footer.php
+++ project/gocart/themes/default/views/footer.php
@@ -1,7 +1,20 @@
 </div>
 <div class="row">
     <div class="col-lg-12 footer">
-
+    	<div class="row text-center">
+    		<div class="col-lg-4">
+    			<br>
+    			<p style="font-weight:200">contacto@tucampoverde.com</p>
+    		</div>
+    		<div class="col-lg-4">
+    			<br>
+    			<p style="font-weight:200">55 67 89 09</p>
+    		</div>
+    		<div class="col-lg-4">
+    			<br>
+    			<p style="font-weight:200">Campo Verde <?php echo date('Y')?></p>
+    		</div>
+    	</div>
     </div>
 </div>
 </body>
